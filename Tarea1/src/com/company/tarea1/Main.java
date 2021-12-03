fbe2e2aefb3a7c7490846dbe8ce45047ead57590
fioreArroyo <fiorella.arroyo.amador@ucreativa.com>
2021-12-02 18:19:29 -0600

    cambios ordenamiento

---
+++ Tarea1/src/com/company/tarea1/Main.java
@@ -0,0 +1,10 @@
+package com.company.tarea1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema para Convertir números a letras");
+        FrontEnd fe = new FrontEnd("Sistema para Convertir números a letras");
+        fe.build();
+    }
+}
